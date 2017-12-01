9144661e1b18f4d90e45b86c63172f6567e60c8b
Milica Jovanovic <j.milica95@yahoo.com>
2017-12-01 19:58:32 +0100

    Logika za logovanje i prikaz podataka na glavnoj stranici

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,4 +1,23 @@
-app.controller('indexController', ['$scope', 'functions', '$http', function($scope, functions, $http) {
- 	// alert("Index");
- 	
+app.controller('indexController', ['$scope', 'functions', '$http', '$location',
+    function($scope, functions, $http, $location) {
+
+    $scope.user = {
+        username : '',
+        password : ''
+    };
+
+    $scope.login = function() {
+        let result = functions.login($scope.user);
+
+        if (result) {
+            let loggedUser = {
+                username : $scope.user.username,
+                loginTime : new Date()
+            }
+            localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+            $location.path('/home');
+        } else {
+            alert("You have entered incorrect data, please try again.");
+        }
+    };
 }]);
